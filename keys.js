f02c5fcb448ffa1ff6ad816922b427afcc129a8c
emilyhosoya <emilyhosoya@gmail.com>
2018-01-21 15:29:11 -0700

    Spotify and OMDB queries are working

--- keys.js
+++ keys.js
@@ -1,10 +1,16 @@
 // console.log("this is loaded");
 
-const twitterKeys = {
-  consumer_key: "cKiBs2ZNG66bhBYiCdwJ2YnbP",
-  consumer_secret: "SdHLkFsAUDKneygabd8UQqwcfyYigSIEYkoKjvW8r6uIO7nJh9",
-  access_token_key: "954879360623828992-w7IUeoTHOE5yudtnUTmSD4p6Z3MMPLy",
-  access_token_secret: "jibBjJrbNVyFbMQyfBhMvpT5uD8BITpLqWRWX4tN959kU"
+const keys = {
+  twitterKeys: {
+    consumer_key: "cKiBs2ZNG66bhBYiCdwJ2YnbP",
+    consumer_secret: "SdHLkFsAUDKneygabd8UQqwcfyYigSIEYkoKjvW8r6uIO7nJh9",
+    access_token_key: "954879360623828992-w7IUeoTHOE5yudtnUTmSD4p6Z3MMPLy",
+    access_token_secret: "jibBjJrbNVyFbMQyfBhMvpT5uD8BITpLqWRWX4tN959kU"
+  },
+  spotifyKeys: {
+    id: "f6be6fd39d904a55855fc22872bd84a9",
+    secret: "fa34356549b24fd19b10c479ca4c9ff8"
+  }
 };
 
-module.exports = twitterKeys;
+module.exports = keys;
--- liri.js
+++ liri.js
@@ -1,16 +1,20 @@
-const appKeys = require("./keys.js");
+const keychain = require("./keys.js");
 
 const Twitter = require("twitter");
-const spotify = require("node-spotify-api");
+const Spotify = require("node-spotify-api");
 const request = require("request");
+const fs = require("fs");
 
-const currentCommand = process.argv[2];
-const optionalArgument = process.argv[3];
+let currentCommand = process.argv[2];
+let optionalArgument = process.argv[3];
 
 const liriBot = {
+  // -----------------------------------------
+  // ---------- TWITTER QUERY ----------------
+  // -----------------------------------------
+  // This will show your last 20 tweets and when they were created at in your terminal/bash window
   "my-tweets": () => {
-    // show your last 20 tweets and when they were created at in your terminal/bash window
-    const client = new Twitter(appKeys);
+    const client = new Twitter(keychain.twitterKeys);
 
     const params = {
       screen_name: "catfishcowgirl9",
@@ -21,7 +25,9 @@ const liriBot = {
       tweets,
       response
     ) {
-      if (!error) {
+      if (error) {
+        return console.log("Error occurred: " + error);
+      } else {
         tweets.forEach(function(tweet) {
           console.log(`
           ------------------------------\n
@@ -33,15 +39,124 @@ const liriBot = {
     });
     console.log("Twitter is working... just wait!");
   },
+  // -----------------------------------------
+  // ----------- SPOTIFY QUERY ---------------
+  // -----------------------------------------
+  // This will show the following information about the song in your terminal/bash window
+  // * Artist(s)
+  // * The song's name
+  // * A preview link of the song from Spotify
+  // * The album that the song is from
+  // If no song is provided then your program will default to "The Sign" by Ace of Base.
   "spotify-this-song": songTitle => {
-    // do stuff
+    const spotify = new Spotify(keychain.spotifyKeys);
+
+    spotify.search(
+      { type: "track", query: songTitle, offset: 0, limit: 1 },
+      function(error, data) {
+        if (error) {
+          return console.log("Error occurred: " + error);
+        } else {
+          const results = data.tracks.items;
+          results.forEach(function(song) {
+            // For songs with multiple artists, add artist names to an array
+            const artistFullData = song.artists;
+            let artistNamesList = [];
+            artistFullData.forEach(function(artist) {
+              let artistName = artist.name;
+              artistNamesList.push(artistName);
+            });
+
+            console.log(`
+            ------------------------------\n
+            Artist: ${artistNamesList.join(", ")}\n
+            Song: ${song.name}\n
+            Song Preview: ${song.preview_url}\n
+            Album: ${song.album.name}\n
+            `);
+          });
+        }
+      }
+    );
+
+    console.log("Spotify is working... just wait!");
     console.log(`Your song: '${process.argv[3]}'`);
   },
+  // -----------------------------------------
+  // -------------- OMDB QUERY ---------------
+  // -----------------------------------------
+  // This will output the following information to your terminal/bash window:
+  // * Title of the movie.
+  // * Year the movie came out.
+  // * IMDB Rating of the movie.
+  // * Rotten Tomatoes Rating of the movie.
+  // * Country where the movie was produced.
+  // * Language of the movie.
+  // * Plot of the movie.
+  // * Actors in the movie.
+  // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
   "movie-this": movieName => {
+    const queryUrl =
+      "http://www.omdbapi.com/?t=" +
+      movieName +
+      "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    request(queryUrl, function(error, response, body) {
+      if (error) {
+        return console.log("Error occurred: " + error);
+      } else {
+        console.log(`
+        ------------------------------\n
+        Title: ${JSON.parse(body).Title}\n
+        Release Year: ${JSON.parse(body).Year}\n
+        IMDB Rating: ${
+          JSON.parse(body).Ratings.find(
+            source => source.Source === "Internet Movie Database"
+          ).Value
+        }\n
+        Rotten Tomatoes Rating: ${
+          JSON.parse(body).Ratings.find(
+            source => source.Source === "Rotten Tomatoes"
+          ).Value
+        }\n
+        Country Produced: ${JSON.parse(body).Country}\n
+        Language: ${JSON.parse(body).Language}\n
+        Plot: ${JSON.parse(body).Plot}\n
+        Actors: ${JSON.parse(body).Actors}\n
+        `);
+      }
+    });
+
     console.log(`Your move: '${process.argv[3]}'`);
+    console.log("Movie request is working... just wait!");
   },
+  // -----------------------------------------
+  // -------------- FS COMMAND ---------------
+  // -----------------------------------------
+  // Take the text inside of random.txt and then use it to call one of LIRI's commands
   "do-what-it-says": () => {
-    console.log("What do you want me to do?");
+    console.log("Running random.txt...");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error) {
+        return console.log(error);
+      } else {
+        const allCommands = data.split("\n");
+
+        allCommands.forEach(function(element) {
+          element.split(",");
+        });
+
+        console.log(allCommands);
+
+        // dataArr.forEach(function(data) {
+        //   currentCommand = data[0];
+        //   optionalArgument = data[1];
+        // });
+        // console.log(currentCommand, optionalArgument);
+      }
+    });
   }
 };
 
